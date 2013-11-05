e4115203d05bf3ea75b7b95fd6e965ffa2a9879b
Adrian Payne <allthenoise@hotmail.com>
2013-11-05 01:59:41 +0100

    Basic Layout
    * Basic framework with slide toggle sidebar menu
    * Fontawesome library files added

---
+++ src/main/webapp/js/brains.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function(){
+
+	
+	var sidebar = $('.sidebar');
+	var container = $('.container');
+	
+	// toggle sidebar on click
+	$('.toggle-aside').on('click',function(e){
+		e.preventDefault();
+		if(sidebar.hasClass('sidebar-open')){
+			sidebar.removeClass('sidebar-open').addClass('sidebar-closed');
+			container.removeClass('container-margin').addClass('container-nomargin');
+		}
+		else{
+			sidebar.removeClass('sidebar-closed').addClass('sidebar-open');
+			container.removeClass('container-nomargin').addClass('container-margin');
+		}
+	});
+
+});
