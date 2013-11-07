b934a2157878806b4bc7b55554fb5ecacd80762e
Ethan Hall <ethan@ehdev.io>
2013-11-06 20:17:12 -0600

    Adding travis and resources

--- src/main/groovy/io/ehdev/easyinvoice/config/WebConfig.groovy
+++ src/main/groovy/io/ehdev/easyinvoice/config/WebConfig.groovy
@@ -5,12 +5,22 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @EnableWebMvc
 @ComponentScan([ "io.ehdev.easyinvoice" ])
 @EnableAsync
 @EnableScheduling
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/")
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/")
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/")
+        registry.addResourceHandler("/html/**").addResourceLocations("/html/")
+    }
 
 }
