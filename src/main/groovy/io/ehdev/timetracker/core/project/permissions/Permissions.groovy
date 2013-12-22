df2a13eb5b176ed727c6f70d995d8db38df62902
Ethan Hall <ethan@ehdev.io>
2013-12-22 09:17:03 -0600

    added parent permissions

--- src/main/groovy/io/ehdev/timetracker/core/project/permissions/Permissions.groovy
+++ src/main/groovy/io/ehdev/timetracker/core/project/permissions/Permissions.groovy
@@ -3,16 +3,23 @@ package io.ehdev.timetracker.core.project.permissions
 import io.ehdev.timetracker.core.user.User
 
 class Permissions {
+
+    def parentPermissions = null
+
     def List<User> readAccess = []
     def List<User> writeAccess = []
 
     boolean canUserWrite(User user){
-        return writeAccess.any{
-            it.getId() == user.getId()
+        if(parentPermissions?.canUserWrite(user)){
+            return true
         }
+        return user in writeAccess
     }
 
     boolean canUserRead(User user){
+        if(parentPermissions?.canUserRead(user)){
+            return true
+        }
         return user in writeAccess || user in readAccess
     }
 }
--- src/test/groovy/io/ehdev/timetracker/core/project/permissions/PermissionsTest.groovy
+++ src/test/groovy/io/ehdev/timetracker/core/project/permissions/PermissionsTest.groovy
@@ -1,33 +1,46 @@
 package io.ehdev.timetracker.core.project.permissions
-
-import io.ehdev.timetracker.core.project.permissions.Permissions
 import io.ehdev.timetracker.core.user.User
-import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 
 import static org.fest.assertions.Assertions.assertThat
 
 class PermissionsTest {
 
-    Permissions permissions;
-    User writeUser;
-    User readUser;
+    private final static User writeUser = User.newInstance(id: "1")
+    private final static User readUser = User.newInstance(id: "2")
 
-    @BeforeMethod
-    public void setup(){
-        writeUser = User.newInstance(id: "1")
-        readUser = User.newInstance(id: "2")
-        permissions = Permissions.newInstance(readAccess: [readUser], writeAccess: [writeUser])
+    private Permissions createPermissionObject(readUser, writeUser){
+        return Permissions.newInstance(readAccess: [readUser], writeAccess: [writeUser])
     }
 
     @Test
     public void testReadPermissions() throws Exception {
+        Permissions permissions = createPermissionObject(readUser, writeUser)
         assertThat(permissions.canUserRead(readUser)).isTrue()
         assertThat(permissions.canUserRead(writeUser)).isTrue()
     }
 
     @Test
     public void testWritePermissions() throws Exception {
+        Permissions permissions = createPermissionObject(readUser, writeUser)
+        assertThat(permissions.canUserWrite(writeUser)).isTrue()
+        assertThat(permissions.canUserWrite(readUser)).isFalse()
+    }
+
+    @Test
+    public void testParentReadPermissions() throws Exception {
+        Permissions parent = createPermissionObject(readUser, writeUser)
+        Permissions permissions = createPermissionObject(null, null)
+        permissions.parentPermissions = parent
+        assertThat(permissions.canUserRead(readUser)).isTrue()
+        assertThat(permissions.canUserRead(writeUser)).isTrue()
+    }
+
+    @Test
+    public void testParentWritePermissions() throws Exception {
+        Permissions parent = createPermissionObject(readUser, writeUser)
+        Permissions permissions = createPermissionObject(null, null)
+        permissions.parentPermissions = parent
         assertThat(permissions.canUserWrite(writeUser)).isTrue()
         assertThat(permissions.canUserWrite(readUser)).isFalse()
     }
