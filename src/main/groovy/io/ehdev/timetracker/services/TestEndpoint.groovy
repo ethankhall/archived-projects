5045d4440d98c7c47ce98ee8586d935963f88973
Ethan Hall <ethan@ehdev.io>
2013-12-28 08:50:09 -0600

    Fixing timestamp

--- src/main/groovy/io/ehdev/timetracker/services/TestEndpoint.groovy
+++ src/main/groovy/io/ehdev/timetracker/services/TestEndpoint.groovy
@@ -1,6 +1,6 @@
 package io.ehdev.timetracker.services
-
 import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -11,7 +11,7 @@ class TestEndpoint {
 
     @RequestMapping(method = RequestMethod.GET)
     public def getExampleData() {
-        return [ createdAt: DateTime.now(), output: 'this is a test resource']
+        return [ createdAt: ISODateTimeFormat.dateTime().print(DateTime.now()), output: 'this is a test resource']
     }
 
 }
