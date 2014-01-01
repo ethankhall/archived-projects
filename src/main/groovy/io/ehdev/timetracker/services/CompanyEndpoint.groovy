6631423bc1921b7e5357b8287c57223ef4c02150
Ethan Hall <ethan@ehdev.io>
2014-01-01 00:08:30 -0600

    Adding some endpoints

--- src/main/groovy/io/ehdev/timetracker/services/CompanyEndpoint.groovy
+++ src/main/groovy/io/ehdev/timetracker/services/CompanyEndpoint.groovy
@@ -1,11 +1,23 @@
 package io.ehdev.timetracker.services
 
+import org.springframework.security.openid.OpenIDAuthenticationToken
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/company")
 class CompanyEndpoint {
 
+    @RequestMapping(method = RequestMethod.POST)
+    public def createNewCompany(OpenIDAuthenticationToken authentication){
+        return "Congratulations, you would have created a company here"
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = '/{uid}')
+    public def updateCompany(@PathVariable('uid') String uid, OpenIDAuthenticationToken authentication){
+        return 'Updated Project'
+    }
 
 }
