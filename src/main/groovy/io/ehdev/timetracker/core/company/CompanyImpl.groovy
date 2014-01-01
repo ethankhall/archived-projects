af9cec3272de30d21155dba0268c66cd7f13a15c
Ethan Hall <ethan@ehdev.io>
2013-12-31 23:04:19 -0600

    Company can now persist (limited)

--- src/main/groovy/io/ehdev/timetracker/core/company/CompanyImpl.groovy
+++ src/main/groovy/io/ehdev/timetracker/core/company/CompanyImpl.groovy
@@ -1,12 +1,22 @@
 package io.ehdev.timetracker.core.company
 import io.ehdev.timetracker.core.PreformActionBaseImpl
 import io.ehdev.timetracker.core.Storable
-import io.ehdev.timetracker.core.permissions.Permissions
+import io.ehdev.timetracker.core.permissions.ExtendedPermissions
 
+import javax.persistence.*
+
+@Table
+@Entity
 class CompanyImpl extends PreformActionBaseImpl implements Company, Storable {
 
+    @Id
+    @GeneratedValue
     Integer id
+
+    @Column
     String name
-    Permissions permissions
+
+    @ManyToOne
+    ExtendedPermissions permissions
 
 }
