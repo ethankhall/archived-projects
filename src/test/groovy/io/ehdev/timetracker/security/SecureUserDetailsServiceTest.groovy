3bafd114c802910e4d6079e132c8b701bbffe262
Ethan Hall <ethan@ehdev.io>
2014-01-04 11:05:46 -0600

    slowly removing spring in tests

--- src/test/groovy/io/ehdev/timetracker/security/SecureUserDetailsServiceTest.groovy
+++ src/test/groovy/io/ehdev/timetracker/security/SecureUserDetailsServiceTest.groovy
@@ -1,19 +1,13 @@
 package io.ehdev.timetracker.security
+import groovy.mock.interceptor.MockFor
 import groovy.util.logging.Slf4j
-import io.ehdev.timetracker.config.HibernateConfig
-import io.ehdev.timetracker.config.PropertyFileLoader
 import io.ehdev.timetracker.core.user.UserBuilder
 import io.ehdev.timetracker.core.user.UserImpl
 import io.ehdev.timetracker.core.user.UserNotFoundException
 import io.ehdev.timetracker.storage.user.UserDaoImpl
-import org.hibernate.SessionFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.openid.OpenIDAttribute
 import org.springframework.security.openid.OpenIDAuthenticationToken
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
@@ -24,15 +18,10 @@ import static org.mockito.Mockito.when
 import static org.testng.collections.Lists.newArrayList
 
 @Slf4j
-@ActiveProfiles("test")
-@ContextConfiguration(classes = [PropertyFileLoader.class, HibernateConfig.class])
-class SecureUserDetailsServiceTest extends AbstractTransactionalTestNGSpringContextTests {
-
-    @Autowired
-    SessionFactory sessionFactory
+class SecureUserDetailsServiceTest {
 
     SecureUserDetailsService service
-    UserDaoImpl userDao
+    MockFor userDao
     OpenIDAuthenticationToken token
 
     @BeforeClass
@@ -45,47 +34,53 @@ class SecureUserDetailsServiceTest extends AbstractTransactionalTestNGSpringCont
     }
 
     @BeforeMethod
-    public void setup() {
-        service = new SecureUserDetailsService()
-        userDao = new UserDaoImpl( sessionFactory: sessionFactory)
-        service.setUserDao(userDao)
+    public void createNewMock() {
+        userDao = new MockFor(UserDaoImpl)
+    }
+
+    private void setupService() {
+        service = new SecureUserDetailsService(userDao: userDao.proxyInstance())
     }
 
     @Test
     public void testLoadUserDetails_whereNoneIsPresent() throws Exception {
+        userDao.demand.with {
+            getUserFromToken(token) { throw new UserNotFoundException('', '') }
+            save { null }
+        }
+        setupService()
+
         UserDetails details = service.loadUserDetails(token)
         assertThat(details.getUsername()).isSameAs("123")
     }
 
     @Test
     public void testLoadUserDetails_whereOneIsPresent() throws Exception {
-        userDao.save(new UserImpl(uuid: "SOMETHING", authToken: "123"))
+        givenUser("123")
+
         UserDetails details = service.loadUserDetails(token)
         assertThat(details.getUsername()).isSameAs("123")
     }
 
-    @Test
-    public void testCreateUser() throws Exception {
-        def user = service.createNewUser(token)
-        assertThat(user.uuid).isNotNull()
-        assertThat(user.name).isEqualTo('doe')
-        assertThat(user.email).isEqualTo('joe@doe.com')
-    }
-
     @Test(expectedExceptions = UserNotFoundException.class)
     public void testGetUser_whereNoneIsPresent() throws Exception {
+        givenUserNotFound()
+
         service.getUser(token)
     }
 
     @Test
     public void testGetUser() throws Exception {
-        userDao.save(new UserImpl(uuid: "SOMETHING", authToken: "123"))
-        service.getUser(token)
+        userDao.demand.getUserFromToken(token) { null }
+        setupService()
+
+        assertThat(service.getUser(token)).isNull()
     }
 
     @Test
     public void testPopulateUserFromOpenId() throws Exception {
         def user = UserBuilder.createNewUser()
+        setupService()
         service.populateUserFromOpenId(user,
                 newArrayList(new OpenIDAttribute('lastname', 'something', ['doe']),
                     new OpenIDAttribute('email', 'something', ['joe@doe.com']),
@@ -96,8 +91,9 @@ class SecureUserDetailsServiceTest extends AbstractTransactionalTestNGSpringCont
     }
 
     @Test
-    public void testPopulateUserFromOpenId_whereFullnameIsMissing() throws Exception {
+    public void testPopulateUserFromOpenId_whereFullNameIsMissing() throws Exception {
         def user = UserBuilder.createNewUser()
+        setupService()
         service.populateUserFromOpenId(user,
                 newArrayList(new OpenIDAttribute('lastname', 'something', ['doe']),
                         new OpenIDAttribute('email', 'something', ['joe@doe.com']),
@@ -108,12 +104,26 @@ class SecureUserDetailsServiceTest extends AbstractTransactionalTestNGSpringCont
 
     @Test(expectedExceptions = UserNotFoundException.class)
     public void testLoadUserByUsername_whereNoUserIsPresent() throws Exception {
+        givenUserNotFound()
+
         service.loadUserByUsername("123")
     }
 
     @Test
     public void testLoadUserByUsername_whereUserIsPresent() throws Exception {
-        userDao.save(new UserImpl(uuid: "SOMETHING", authToken: "123"))
+        givenUser("123")
+
         service.loadUserByUsername("123")
     }
+
+    private void givenUserNotFound(){
+        userDao.demand.getUserFromToken(token) { throw new UserNotFoundException('', '') }
+        setupService()
+    }
+
+    public void givenUser(String userToken) {
+        userDao.demand.getUserFromToken(token) { return new UserImpl(authToken: userToken) }
+        setupService()
+    }
+
 }
