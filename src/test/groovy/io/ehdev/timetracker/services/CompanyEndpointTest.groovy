20342d8e0207490830a003e9f901b599fbfe7b72
Ethan Hall <ethan@ehdev.io>
2014-01-04 11:29:30 -0600

    more refactoring

--- src/test/groovy/io/ehdev/timetracker/services/CompanyEndpointTest.groovy
+++ src/test/groovy/io/ehdev/timetracker/services/CompanyEndpointTest.groovy
@@ -1,8 +1,7 @@
 package io.ehdev.timetracker.services
 import groovy.mock.interceptor.MockFor
 import io.ehdev.timetracker.TokenBuilder
-import io.ehdev.timetracker.core.company.CompanyImpl
-import io.ehdev.timetracker.core.permissions.UserCompanyPermissions
+import io.ehdev.timetracker.core.company.CompanyInteractor
 import io.ehdev.timetracker.core.user.UserBuilder
 import io.ehdev.timetracker.core.user.UserImpl
 import io.ehdev.timetracker.services.external.company.ExternalCompany
@@ -23,8 +22,8 @@ class CompanyEndpointTest {
     def companyDao
     def userCompanyPermissionsDao
 
-    private UserImpl user1
-    private UserImpl user2
+    private UserImpl authorizedUser
+    private UserImpl otherUser
 
     @BeforeMethod
     public void setup(){
@@ -32,42 +31,37 @@ class CompanyEndpointTest {
         companyDao = new MockFor(CompanyDaoImpl)
         userCompanyPermissionsDao = new MockFor(UserCompanyPermissionsDaoImpl)
 
-        user1 = UserBuilder.createNewUser()
-        user2 = UserBuilder.createNewUser()
+        authorizedUser = UserBuilder.createNewUser()
+        otherUser = UserBuilder.createNewUser()
     }
 
     @Test
     public void testCreatingACompany() throws Exception {
-        userDao.demand.getUserFromToken { user1 }
-        companyDao.demand.save { null }
         setupService()
 
-        def company = createCompanyWithEndpoint('something', user1)
+        def company = companyEndpoint.createNewCompany(
+                ExternalCompanyBuilder.createCompany([name: 'something']),
+                TokenBuilder.build(authorizedUser))
 
         assertThat(company.getUuid()).isNotNull()
         assertThat(company.getName()).isEqualTo('something')
         assertThat(company.admin).hasSize(1)
-        assertThat(company.admin).containsOnly(ExternalUser.convertUser(user1))
+        assertThat(company.admin).containsOnly(ExternalUser.convertUser(authorizedUser))
     }
 
     @Test
     public void testGetCreatedCompany() throws Exception {
-        def company = createCompany("something", "something", user1)
+        def company = CompanyInteractor.createNewCompany(authorizedUser, "something")
 
-        userDao.demand.getUserFromToken { user1 }
-        companyDao.demand.getByUuid('something') { company }
-
+        companyDao.demand.getByUuid(company.uuid) { company }
         setupService()
 
-        def retrievedCompany = companyEndpoint.getCompany("something", null)
+        def retrievedCompany = companyEndpoint.getCompany(company.uuid, null)
         assertThat(retrievedCompany).isEqualTo(new ExternalCompany(company))
     }
 
     @Test
     public void testGettingCompaniesForUser_whereThereWereNoneForUser() throws Exception {
-        userDao.demand.getUserFromToken { user1 }
-        userCompanyPermissionsDao.demand.getCompaniesAvailableToUser(user2) { [] }
-
         setupService()
 
         assertThat(companyEndpoint.getAllCompaniesForUser(null)).isEmpty()
@@ -75,38 +69,25 @@ class CompanyEndpointTest {
 
     @Test
     public void testGettingCompaniesForUser() throws Exception {
+        def company1 = CompanyInteractor.createNewCompany(authorizedUser, "something")
+        def company2 = CompanyInteractor.createNewCompany(authorizedUser, "something2")
 
-        def company1 = createCompany("something", "something", user1)
-        def company2 = createCompany("something2", "something2", user1)
-
-        userDao.demand.getUserFromToken { user1 }
-        userCompanyPermissionsDao.demand.getCompaniesAvailableToUser(user1) { [company1, company2] }
-
+        setupGetCompanyReturns([company1, company2])
         setupService()
 
         def companies = companyEndpoint.getAllCompaniesForUser(null)
         assertThat(companies).containsOnly(new ExternalCompany(company1), new ExternalCompany(company2))
     }
 
-    public CompanyImpl createCompany(String name, String uuid, UserImpl user) {
-        def company = new CompanyImpl(
-                name: name,
-                uuid: uuid)
-        def permissions = [ new UserCompanyPermissions(
-                refUser: user,
-                adminAccess: true,
-                company: company) ]
-        company.setPermissions(permissions)
-        return company
-    }
-
-    public ExternalCompany createCompanyWithEndpoint(String name, UserImpl user) {
-        companyEndpoint.createNewCompany(
-                ExternalCompanyBuilder.createCompany([name: name]),
-                TokenBuilder.build(user))
+    private void setupGetCompanyReturns( authorizedUser ){
+        userCompanyPermissionsDao.demand.getCompaniesAvailableToUser(otherUser) { authorizedUser }
     }
 
     private void setupService() {
+        userDao.demand.getUserFromToken { authorizedUser }
+        companyDao.demand.save { null }
+        userCompanyPermissionsDao.demand.getCompaniesAvailableToUser(otherUser) { [] }
+
         companyEndpoint = new CompanyEndpoint(
                 userDao: userDao.proxyInstance(),
                 companyDao: companyDao.proxyInstance(),
